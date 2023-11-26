537a1b3636e0749390b34a7d580e9360f92ef37f
Surendra <manjhisurendra08@gmail.com>
2023-11-26 18:35:40 +0530

    Fetching data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { useContext } from "react";
 import "./App.css";
+import Card from "./Components/Card/Card";
+import { Context } from "./Context/context";
 
 function App() {
-	
+	const context = useContext(Context);
+
+	console.log(context);
+
 	return (
 		<div className="App">
 			<h1>DOG.</h1>
+			<Card />
 		</div>
 	);
 }
--- src/Context/context.jsx
+++ src/Context/context.jsx
@@ -1,7 +1,34 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const Context = createContext(null);
 
 export function ContextProvider(props) {
-	return <Context.Provider value={{}}>{props.children}</Context.Provider>;
+	const [data, setData] = useState(4);
+
+	async function fetchData(params) {
+		const url =
+			"https://dog-breeds2.p.rapidapi.com/dog_breeds/breed/German";
+		const options = {
+			method: "GET",
+			headers: {
+				"X-RapidAPI-Key":
+					"6c67610f29msh795c97cf851fa6cp1b4429jsn5726ef1d904b",
+				"X-RapidAPI-Host": "dog-breeds2.p.rapidapi.com",
+			},
+		};
+
+		try {
+			const response = await fetch(url, options);
+			const result = await response.json();
+			setData(result);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	return (
+		<Context.Provider value={{ data, fetchData }}>
+			{props.children}
+		</Context.Provider>
+	);
 }
